365108176a443d6438ca88f3dca5e5bb0381b653
PabloVillapunMartin <pvillapunmartin@gmail.com>
2023-07-26 20:56:53 +0200

    Added movement functionality + some assets moved

---
+++ Assets/Scripts/SpaceshipDebugHUD.cs
@@ -0,0 +1,37 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpaceshipDebugHUD : MonoBehaviour
+{
+    [Header("Spaceship movement")]
+    [SerializeField] private Image _innerImage;
+    [SerializeField] private Image _outerImage;
+    [SerializeField] private SpaceshipMovement _spaceshipMovement;
+    [SerializeField] private TextMeshProUGUI _velocityText;
+    [SerializeField] private TextMeshProUGUI _thrustText;
+    
+    private RectTransform _innerRect;
+    private float radius;
+
+    private void Start()
+    {
+        _innerRect = _innerImage.GetComponent<RectTransform>();
+
+        RectTransform _outerRect = _outerImage.GetComponent<RectTransform>();
+        radius = _outerRect.rect.width * 0.5f - _innerRect.rect.width * 0.5f;
+    }
+
+    private void Update()
+    {
+        if (_spaceshipMovement == null) return;
+
+        Vector2 input = _spaceshipMovement.GetDirectionDelta();
+        input.Normalize();
+
+        _innerRect.anchoredPosition = input * radius;
+
+        _velocityText.text = "Velocity: " + _spaceshipMovement.GetVelocity().magnitude.ToString();
+        _thrustText.text = "Thrust Velocity: " + _spaceshipMovement.GetThrust().ToString();
+    }
+}
--- Assets/Scripts/SpaceshipMovement.cs
+++ Assets/Scripts/SpaceshipMovement.cs
@@ -5,40 +5,33 @@ using UnityEngine.InputSystem;
 public class SpaceshipMovement : MonoBehaviour
 {
     [Header("Spaceship settings")]
-    [SerializeField]
-    private float _yawTorque = 500f;
-    [SerializeField]
-    private float _pitchTorque = 1000f;
-    [SerializeField]
-    private float _rollTorque = 1000f;
-    [SerializeField]
-    private float _thrust = 100f;
-    [SerializeField]
-    private float _upthrust = 50f;
-    [SerializeField]
-    private float _strafeThrust = 50f;
-    [SerializeField, Range(0.001f, 0.999f)]
-    private float _thrustGlideReduction = 0.999f;
-    [SerializeField, Range(0.001f, 0.999f)]
-    private float _upDownGlideReduction = 0.111f;
-    [SerializeField, Range(0.001f, 0.999f)]
-    private float _leftRigtGlideReduction = 0.111f;
+
+    [SerializeField] private float _yawTorque = 500f;
+    [SerializeField] private float _pitchTorque = 1000f;
+    [SerializeField] private float _rollTorque = 1000f;
+    [SerializeField] private float _thrustForce = 100f;
+
+    [Tooltip("Ammount of thrust reduction if the ship is not accelerating")]
+    [SerializeField, Range(0.001f, 0.999f)] private float _thrustReduction = 0.5f;
+
+    [SerializeField] private float _maxVelocity = 100f;
+
+    private float _spaceshipVelocity;
 
     private Rigidbody _rb;
-    private float _glide = 0f;
-    private float _verticalGlide = 0f;
-    private float _horizontalGlide = 0f;
 
     // Input values
     private float _thrust1D;
-    private float _upDown1D;
-    private float _strafe1D;
     private float _roll1D;
     private Vector2 _pitchYaw;
 
     private void Start()
     {
+        _spaceshipVelocity = 0f;
         _rb = GetComponent<Rigidbody>();
+        _rb.maxAngularVelocity = 1f;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     private void FixedUpdate()
@@ -58,57 +51,45 @@ public class SpaceshipMovement : MonoBehaviour
         // Thrust
         if(_thrust1D > 0.1f || _thrust1D < -0.1f)
         {
-            _rb.AddRelativeForce(Vector3.forward * _thrust1D * _thrust * Time.fixedDeltaTime);
-            _glide = _thrust;
+            _spaceshipVelocity = Mathf.Clamp(_spaceshipVelocity + _thrustForce * Time.fixedDeltaTime, 0f, _maxVelocity);
         }
         else
         {
-            _rb.AddRelativeForce(Vector3.forward * _glide * Time.fixedDeltaTime);
-            _glide *= _thrustGlideReduction;
+            _spaceshipVelocity -= _spaceshipVelocity * _thrustReduction * Time.fixedDeltaTime;
+            _spaceshipVelocity = Mathf.Clamp(_spaceshipVelocity, 0f, _maxVelocity);
         }
 
-        // Up/down 
-        if (_upDown1D > 0.1f || _upDown1D < -0.1f)
+        _rb.velocity += transform.forward * _spaceshipVelocity * Time.fixedDeltaTime;
+        if(_rb.velocity.magnitude >= _maxVelocity)
         {
-            float verticalGlide = _upDown1D * _upthrust;
-            _rb.AddRelativeForce(Vector3.up * verticalGlide * Time.fixedDeltaTime);
-            _verticalGlide = verticalGlide;
-        }
-        else
-        {
-            _rb.AddRelativeForce(Vector3.up * _verticalGlide * Time.fixedDeltaTime);
-            _verticalGlide *= _upDownGlideReduction;
-        }
-
-        // Strafing
-        if (_strafe1D > 0.1f || _strafe1D < -0.1f)
-        {
-            float horizontalGlide = _strafe1D * _strafeThrust;
-            _rb.AddRelativeForce(Vector3.right * horizontalGlide * Time.fixedDeltaTime);
-            _horizontalGlide = horizontalGlide;
-        }
-        else
-        {
-            _rb.AddRelativeForce(Vector3.right * _horizontalGlide * Time.fixedDeltaTime);
-            _horizontalGlide *= _leftRigtGlideReduction;
+            _rb.velocity = _rb.velocity.normalized * _maxVelocity;
         }
     }
+
+    #region Public Methods
+
+    public Vector2 GetDirectionDelta()
+    {
+        return _pitchYaw;
+    }
+
+    public Vector3 GetVelocity()
+    {
+        return _rb.velocity;
+    }
+
+    public float GetThrust()
+    {
+        return _spaceshipVelocity;
+    }
+
+    #endregion
 
     #region Input Methods
 
     public void OnThrust(InputAction.CallbackContext context)
     {
         _thrust1D = context.ReadValue<float>();
-    }
-
-    public void OnStrafe(InputAction.CallbackContext context)
-    {
-        _strafe1D = context.ReadValue<float>();
-    }
-
-    public void OnUpDown(InputAction.CallbackContext context)
-    {
-        _upDown1D = context.ReadValue<float>();
     }
 
     public void OnRoll(InputAction.CallbackContext context)
